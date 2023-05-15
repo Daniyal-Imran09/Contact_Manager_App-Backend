815d3b73b66cdeb1fd7b08350db55308881a838e
Daniyal-Imran09 <daniyalimran602@gmail.com>
2023-05-15 18:11:50 +0500

    Create contactmodel.js

---
+++ models/contactmodel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+
+    userid:{
+       type: mongoose.Schema.Types.ObjectId,
+       required:true,
+       ref:"user"
+    },
+    name:{
+        type: String,
+        required:[true,"please add name"]
+    },
+    email:{
+        type: String,
+        required:[true,"please add email"]
+    },
+    phone:{
+        type: String,
+        required:[true,"please add phone"]
+    }
+},{
+    timestamps:true
+}
+)
+
+module.exports = mongoose.model("contacts",schema)
